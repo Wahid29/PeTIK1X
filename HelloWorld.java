80028c9b149d5c00f365ba8878e3f6d930c45741
Lucky Abdillah <94025775+luckyabdillah@users.noreply.github.com>
2022-01-18 09:44:48 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("nama saya : ujang");
+        System.out.println("nama saya : uep");
         System.out.println("alamat saya : aceh");
         System.out.println("no.hp : 088888888");
      

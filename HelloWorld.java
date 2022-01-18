5ebbad71c253950055bb1baa45a0729d85ff4939
Lucky Abdillah <94025775+luckyabdillah@users.noreply.github.com>
2022-01-18 09:51:52 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("nama saya : uep");
         System.out.println("alamat saya : aceh");
         System.out.println("no.hp : 088888888");
+        System.out.println("Saya sangat senang saat mendapat diskon di indomaret, tetapi saya menyesal tidak ikut main bersama teman-teman saya.")
      
     }

6abb84b95c89c5cadda335b95dc81f5b7a148eed
Wahid29 <97927051+Wahid29@users.noreply.github.com>
2022-01-18 09:16:36 +0700

    Create HelloWorld.java
    Hanya membuat output Hello World menggunakan bahasa java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("nama saya : ujang");
+        System.out.println("alamat saya : aceh");
+        System.out.println("no.hp : 088888888");
+     
+    }

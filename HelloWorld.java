f98f78949cd8efdbc67f8bac80449fc40124996e
Anugrah Trihardi <97927028+Agawinata@users.noreply.github.com>
2022-02-14 14:51:08 +0700

    hapus no hp

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("nama saya : mia khalifa");
         System.out.println("alamat saya : aceh");
-        System.out.println("no.hp : 0808293737");
         System.out.println("Saya sangat senang saat mendapat diskon di indomaret, tetapi saya menyesal tidak ikut main bersama teman-teman saya.");
         System.out.println("OneDag");
     }

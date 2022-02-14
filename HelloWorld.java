bb5892106b2900d2d3fd61f50a5d8b57a516a55d
Anugrah Trihardi <97927028+Agawinata@users.noreply.github.com>
2022-02-14 14:35:15 +0700

    menggati nama, nambah output OneDag

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
    public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("nama saya : uep");
+        System.out.println("nama saya : mia khalifa");
         System.out.println("alamat saya : aceh");
-        System.out.println("no.hp : 088888888");
-        System.out.println("Saya sangat senang saat mendapat diskon di indomaret, tetapi saya menyesal tidak ikut main bersama teman-teman saya.")
-     
+        System.out.println("no.hp : 0808293737");
+        System.out.println("Saya sangat senang saat mendapat diskon di indomaret, tetapi saya menyesal tidak ikut main bersama teman-teman saya.");
+        System.out.println("OneDag");
     }

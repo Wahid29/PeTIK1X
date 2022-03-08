59c20868638ec53e389a61acbf44d2b06a129fdc
Lucky Abdillah <94025775+luckyabdillah@users.noreply.github.com>
2022-03-08 09:16:08 +0700

    Menambahkan hey yo!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Apa kabar mafakeh?")
-        System.out.println("Apa kabar mafakeh cuy?")
-     
+        System.out.println("Apa kabar mafakeh?");
+        System.out.println("Apa kabar mafakeh cuy?");
+        System.out.println("Hey Yo!");
     }
